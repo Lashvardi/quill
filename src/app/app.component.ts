2bcb18e63fac79079bc34323515481aa6c0f2c9d
Lasha <lashakakashvili114@gmail.com>
2023-06-27 16:55:20 +0400

    Implemented Some More Image Styling Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -87,6 +87,21 @@ export class AppComponent implements OnInit {
     // ? Add Bold
   ];
 
+  // ? Border Style Options
+  borderStyleOptions = [
+    { label: 'Solid', value: 'solid' },
+    { label: 'Dashed', value: 'dashed' },
+    { label: 'Dotted', value: 'dotted' },
+    { label: 'Double', value: 'double' },
+    { label: 'Groove', value: 'groove' },
+    { label: 'Ridge', value: 'ridge' },
+    { label: 'Inset', value: 'inset' },
+    { label: 'Outset', value: 'outset' },
+    { label: 'None', value: 'none' },
+    { label: 'Hidden', value: 'hidden' },
+
+  ]
+
   //? Fonf Family Options
   defaultFontFamilies: string[] = [
     'Arial',
